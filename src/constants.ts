b3b1a37a48c79d8139e7d489aae2ca7042efa40c
utk09-NCL <utkarsh.t0298@gmail.com>
2026-08-30 20:59:44 +0100

    chore: match the telemetry sdk version to the package

--- src/constants.ts
+++ src/constants.ts
@@ -234,7 +234,7 @@ export const TELEMETRY_SDK_NAME = "@utk09/ui-observability";
  * Value of `telemetry.sdk.version`. A literal, not read from package.json. Bump by hand alongside package.json.
  * @see {@link RESOURCE_TELEMETRY_SDK_VERSION}
  */
-export const TELEMETRY_SDK_VERSION = "0.1.0";
+export const TELEMETRY_SDK_VERSION = "0.1.1";
 
 /**
  * Value of `telemetry.sdk.language`.
